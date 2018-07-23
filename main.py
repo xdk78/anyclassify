c4b306b143064fe42f7705ed055d91d162d1f1cb
xdk78 <xdk78@interia.pl>
2018-07-23 18:51:36 +0200

    Prepare basic CNN model structure

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import tensorflow as tf
+from tensorflow import keras
 
-hello = tf.constant('Hello, TensorFlow!')
-sess = tf.Session()
-print(sess.run(hello))
+# 0 - anime, 1 - human
+lables = ['anime', 'human']
+model = keras.Sequential([
+    keras.layers.Conv2D(
+        20, (5, 5),
+        padding='same',
+        input_shape=(3, 100, 100)),  # 3 for RGB, image 100 x 100, we need to convert 2D to 1D
+    keras.layers.Dense(
+        units=128,  # hidden neurons
+        activation=tf.nn.relu),
+    keras.layers.Dense(
+        units=10,  # output neurons
+        activation=tf.nn.softmax)
+])
+print(model.input_shape)
+print(model.output_shape)
+# sess = tf.Session()
+# print(sess.run())
