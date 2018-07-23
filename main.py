cea4168bb772cc72d866bc16655c6106b553454c
xdk78 <xdk78@interia.pl>
2018-07-23 17:01:39 +0200

    Fix example

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-    import tensorflow as tf
+import tensorflow as tf
 
-    hello = tf.constant('Hello, TensorFlow!')
-    sess = tf.Session()
-    print(sess.run(hello))
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.Session()
+print(sess.run(hello))
